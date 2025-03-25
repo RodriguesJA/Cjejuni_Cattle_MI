163950aa45051facf1e56bf9e0fd9e250130f6e6
Jose Rodrigues, D.O., Ph.D. <wlp9007@nyp.org>
2025-03-25 11:26:41 -0400

    Update amrfinder.py

--- scripts/amrfinder.py
+++ scripts/amrfinder.py
@@ -1,7 +1,7 @@
 from decimal import *
 import os
 
-Genomes_file = open( "/mnt/gs21/scratch/rodri651/campy_ids.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
+Genomes_file = open( "campy_ids.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
 Genomes_pair = Genomes_file.readlines() #Here you are reading all of the file names within a specific folder readlines enables this fx 
 
 for i in Genomes_pair: #Here we are stripping tabs and spaces from file names 
@@ -10,5 +10,5 @@ for i in Genomes_pair: #Here we are stripping tabs and spaces from file names
 	print (i)
 	pairGenome = i.split("\t")  
 	RefGenome = str(pairGenome[0])  #Here we place all of the integers within our file that we read and stripped into an array. In this case each TW# Will be in an array that is called RefGenome
-	os.system("amrfinder -n /mnt/gs21/scratch/rodri651/genomes_allcj/"+RefGenome+".fasta -O Campylobacter --plus --name" + " " +RefGenome+" --nucleotide_output /mnt/gs21/scratch/rodri651/campy_outputs/amrfinderplus/"+RefGenome+"_amr.fasta -o /mnt/gs21/scratch/rodri651/campy_outputs/amrfinderplus/"+RefGenome+".tsv")
+	os.system("amrfinder -n /"+RefGenome+".fasta -O Campylobacter --plus --name" + " " +RefGenome+" --nucleotide_output /amrfinderplus/"+RefGenome+"_amr.fasta -o campy_outputs/amrfinderplus/"+RefGenome+".tsv")
 	print(RefGenome + "_DONE")
