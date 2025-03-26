f99159963cc38b2afac485f57e580cab523223b5
Jose Rodrigues, D.O., Ph.D. <wlp9007@nyp.org>
2025-03-26 09:50:46 -0400

    Add files via upload

---
+++ scripts/prokka_JAR.py
@@ -0,0 +1,16 @@
+#Prokka annotated of all C. jejuni scripts. Here using a quick python script looped through all genomes in a specific directory
+import os
+import urllib
+import re
+
+Genomes_file = open( "/mnt/gs18/scratch/groups/manninglab/cattlegenomes.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
+Genomes_pair = Genomes_file.readlines() #Here you are reading all of the file names within a specific folder readlines enables this fx 
+
+for i in Genomes_pair: #Here we are stripping tabs and spaces from file names 
+	i = i.rstrip("\n")
+	i = i.rstrip("\r") 
+	print (i)
+	pairGenome = i.split("\t")  
+	RefGenome = str(pairGenome[0])  #Here we place all of the integers within our file that we read and stripped into an array. In this case each TW# Will be in an array that is called RefGenome
+	os.system("prokka  --outdir /mnt/gs18/scratch/groups/manninglab/cattle_prokka_results/" + RefGenome + " --prefix "  +  RefGenome   + "   /mnt/gs18/scratch/groups/manninglab/cattle_genomes/" + RefGenome + ".fasta --compliant") #Here we are using Prokka to annotate genomes see https://github.com/tseemann/prokka
+	print (RefGenome + "_DONE")    #The above loop using the shell command to inact prokka while out putting the the files in the directory .../Prokka as well as appending the RefGenome loop. Therefore each TW# will run through this loop and the last line prints which TW# is being utilized 
